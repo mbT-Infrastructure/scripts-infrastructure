47c1664fabb4a4bdedc812a8d34eba275211d307
madebytimo <t.schnaible@madebytimo.de>
2024-07-25 21:35:55 +0200

    fix logging of username

--- scripts/configure-user.sh
+++ scripts/configure-user.sh
@@ -52,7 +52,7 @@ while [[ -n "$1" ]]; do
     shift
 done
 
-echo "Configuring user \"$USERNAME\" on device \"$DEVICE\"."
+echo "Configuring user \"$USER_USERNAME\" on device \"$DEVICE\"."
 
 "${SCRIPT_DIR}/device-run-command.sh" --device "$DEVICE" --command \
     "(adduser $USER_USERNAME --disabled-password --gecos '' || true ) \
